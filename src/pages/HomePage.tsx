4f54a9d121c70067ec6437f3e60d69557bfd1019
merlinnerd922 <richardb.chen@utoronto.ca>
2026-06-29 09:37:59 -0400

    Updating with next date

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -8,7 +8,7 @@ export default function HomePage() {
             <hr/>
             <p className={colHomeInfoClass}><strong>When: </strong>July 24, 2026, 6pm - 9pm</p>
             <p className={colHomeInfoClass}><strong>Where: </strong>Downtown Toronto</p>
-            <p className={colHomeInfoClass}><strong>Address: </strong>TBA, released on our event's Telegram channel
+            <p className={colHomeInfoClass}><strong>Address: </strong>TBA, released in a private event-related Telegram channel
                 after the event verifies you</p>
             <p className={colHomeInfoClass}><strong>Telegram: </strong><a>https://t.me/CoLPride35</a></p>
         </div>
