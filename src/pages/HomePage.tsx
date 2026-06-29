e8a9c7e25adc7e6949a91ef43c27123d4c69c02e
merlinnerd922 <richardb.chen@utoronto.ca>
2026-06-29 09:18:01 -0400

    Updating with next date

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -6,7 +6,7 @@ export default function HomePage() {
         <div id="col_home">
             <h1>Next event details:</h1>
             <hr/>
-            <p className={colHomeInfoClass}><strong>When: </strong>TBA</p>
+            <p className={colHomeInfoClass}><strong>When: </strong>July 24, 2026, 6pm - 9pm</p>
             <p className={colHomeInfoClass}><strong>Where: </strong>Downtown Toronto</p>
             <p className={colHomeInfoClass}><strong>Address: </strong>TBA, released on our event's Telegram channel
                 after the event verifies you</p>
